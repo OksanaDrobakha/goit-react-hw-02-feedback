48bd9e951c01e1b3cbc8e74d0bffa8a13a4620b3
OksanaDrobakha <6249784@gmail.com>
2023-04-25 22:19:47 +0400

    finish hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
-// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-// import { SectionTitle } from './SectionTitle/SectionTitle';
-// import { Statistics } from './Statistics/Statistics';
-import Counter from './Counter';
+import React from 'react';
 import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = {
@@ -10,7 +11,54 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
+  updateProp = type => {
+    this.setState(prevStat => {
+      let nextState = {};
+      nextState[type] = prevStat[type] + 1;
+      return nextState;
+    });
+  };
+
+  Total = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  positiveFeedback = () => {
+    const { good } = this.state;
+    const total = this.Total();
+    if (total === 0) {
+      return 0;
+    }
+    const positivePercent = (good / total) * 100;
+    return Math.round(positivePercent);
+  };
+
   render() {
-    return <Counter />;
+    return (
+      <div>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.updateProp}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          {this.Total() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.Total()}
+              positivePercentage={this.positiveFeedback()}
+            />
+          )}
+        </Section>
+      </div>
+    );
   }
 }
